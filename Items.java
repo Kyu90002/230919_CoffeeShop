4aa324621755a1566c80482635aad9edb62808d6
Kyumin Lee <2km4890@gmail.com>
2023-09-19 22:59:05 -0500

    done with items class

--- Items.java
+++ Items.java
@@ -2,5 +2,14 @@ class Items {
     public String name;
     public int price;
 
-    
+    public Items() {}
+
+    public Items(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void print_info() {
+        System.out.printf("%s is $%d ", name, price);
+    }
 }
