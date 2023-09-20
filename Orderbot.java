fbd6721fa1aca4802f403cff31f704aa49c8134b
Kyumin Lee <2km4890@gmail.com>
2023-09-19 22:14:58 -0500

    created main & yes_or_no method.

---
+++ Orderbot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Orderbot {
+    public static void main(String args) {
+        
+		Scanner scnr = new Scanner(System.in);
+		String userInput;
+        System.out.println("Would you like to order?");
+		userInput = scnr.nextLine();
+		
+		
+
+    }
+
+	public static boolean yes_or_no(String userInput) {
+
+
+		return false;
+	}
+}
