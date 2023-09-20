54cedd10353e5b2e934b5e4bcff27720fc7c0ab8
Kyumin Lee <2km4890@gmail.com>
2023-09-19 22:27:52 -0500

    finished yes_or_no and edited main

--- Orderbot.java
+++ Orderbot.java
@@ -5,16 +5,19 @@ public class Orderbot {
         
 		Scanner scnr = new Scanner(System.in);
 		String userInput;
+		System.out.println("Hi, welcome to Leefy coffy.");
         System.out.println("Would you like to order?");
 		userInput = scnr.nextLine();
+		
 		
 		
 
     }
 
 	public static boolean yes_or_no(String userInput) {
-
-
+		if (userInput.charAt(0) == 'Y' || userInput.charAt(0) == 'y') {
+			return true;
+		}
 		return false;
 	}
 }
